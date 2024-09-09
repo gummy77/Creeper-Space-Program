c9b59eb1fec60f0ccda4d2b9aa2adc9ad93fe2ac
gummydummy77 <elizabeth.lawler2003@gmail.com>
2024-09-09 18:35:23 +1200

    Initialized Java Workspace through IDEA

---
+++ src/client/java/org/gum/csp/client/CspMainClient.java
@@ -0,0 +1,10 @@
+package org.gum.csp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CspMainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/gum/csp/CspMain.java
@@ -0,0 +1,10 @@
+package org.gum.csp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CspMain implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
