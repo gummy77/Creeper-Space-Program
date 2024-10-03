0622b89da9a4eb1059e831d12008c42d46716af1
Madi <jre129@uclive.ac.nz>
2024-10-03 18:05:18 +1300

    Added pages to the book, added advancement for launching first firework. Changed PartMaterial to have formatted strings

--- src/main/java/org/gum/csp/datastructs/PartMaterial.java
+++ src/main/java/org/gum/csp/datastructs/PartMaterial.java
@@ -4,20 +4,27 @@ package org.gum.csp.datastructs;
  * Enum Class to represent the Materials a rocket part can be
  */
 public enum PartMaterial {
-    NONE(-2),
-    BASE(-1),
-    BAMBOO(0),
-    WOOD(1),
-    COPPER(2),
-    IRON(3);
+    NONE(-2, "None"),
+    BASE(-1, "Base"),
+    BAMBOO(0, "Bamboo"),
+    WOOD(1, "Wooden"),
+    COPPER(2, "Copper"),
+    IRON(3, "Iron");
 
     private final int value;
+    private final String formattedName;
 
-    PartMaterial(int value) {
+    PartMaterial(int value, String formattedName) {
         this.value = value;
+        this.formattedName = formattedName;
     }
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public String toString() {
+        return formattedName;
     }
 }
